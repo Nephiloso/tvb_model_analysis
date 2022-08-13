87ad975e68a3e070865db8ac01c92b1182d68b42
nanda2502 <n.r.jafarian@student.vu.nl>
2022-08-13 21:13:53 +0200

    Revert "Update setup.py"
    This reverts commit b0b0a8ed7ecd7a763a83c550dd03e48b0e88ec88.

--- model/setup.py
+++ model/setup.py
@@ -1,11 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-      name = 'Crit',
-      version = 1.0.0,
-      url ='',
-      description = '',
-      packages = find_packages
-      install_requires = [
-          Crit @ git +ssh://git@github.com/nanda2502/Crit.git]
+    ...
+      dependency_links=['http://github.com/nanda2502/Crit/tarball/master#egg=package-1.0']
+    ...
 )
